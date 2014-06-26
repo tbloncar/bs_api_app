0bc166722cdf1d9ab17e531ac305a74798296970
Travis Loncar <loncar.travis@gmail.com>
2014-06-25 21:05:47 -0400

    Fix tests.

--- spec/unit/budget_simple_api_spec.rb
+++ spec/unit/budget_simple_api_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../../lib/budget_simple_api'
 
 describe BudgetSimpleAPI do
-  let(:api) { BudgetSimpleAPI.new("travis@example.com", "password") }
+  let(:api) { BudgetSimpleAPI.new(email: "travis@example.com", password: "password") }
 
   it 'should be instantiated with an email and password' do
     expect(api.class).to eq(BudgetSimpleAPI)
